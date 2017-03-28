3f20f8d7fdee1ab9bc2904d044473d8df9b84249
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-03-29 00:45:30 +0700

    route match get post kontak

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('kontak', function () {
-    return '<h1>Halaman Kontak</h1>';
-});
+// Route::get('kontak', function () {
+//     return '<h1>Halaman Kontak</h1>';
+// });
+//
+// Route::post('kontak-post', function() {
+//     return 'Anda mengirim pesan "' . $_POST['pesan'] . '"';
+// });
 
-Route::post('kontak-post', function() {
-    return 'Anda mengirim pesan "' . $_POST['pesan'] . '"';
+Route::match(['get', 'post'], 'kontak', function() {
+    $html = '<h1>halaman kontak</h1>';
+    if (isset($_REQUEST['pesan'])) {
+    $html .= 'Anda mengirim pesan ' . $_REQUEST['pesan'];
+    }
+    return $html;
 });
